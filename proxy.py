265571da29bc325d34fe520d6afec6f508e77c80
alexme89 <54217200+alexme89@users.noreply.github.com>
2025-09-14 16:21:34 -0600

    Update proxy.py

--- proxy.py
+++ proxy.py
@@ -1,31 +1,18 @@
-from fastapi import FastAPI, File, UploadFile
-from fastapi.middleware.cors import CORSMiddleware
+import os
 import requests
+from fastapi import FastAPI, File, UploadFile
 
 app = FastAPI()
 
-# Permitir conexiones desde cualquier origen
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],
-    allow_methods=["*"],
-    allow_headers=["*"]
-)
+HUGGINGFACE_API_URL = "https://api-inference.huggingface.co/models/openai/whisper-small"
+HUGGINGFACE_API_KEY = os.environ.get("HUGGINGFACE_API_KEY")
 
-# URL del modelo de Hugging Face
-HF_API_URL = "https://api-inference.huggingface.co/models/openai/whisper-small"
-import os
-HF_TOKEN = os.getenv("HF_TOKEN")
-
-
-headers = {"Authorization": f"Bearer {HF_TOKEN}"}
+headers = {"Authorization": f"Bearer {HUGGINGFACE_API_KEY}"}
 
 @app.post("/process_audio/")
 async def process_audio(file: UploadFile = File(...)):
     audio_bytes = await file.read()
-    response = requests.post(HF_API_URL, headers=headers, data=audio_bytes)
-    try:
-        return response.json()
-    except:
+    response = requests.post(HUGGINGFACE_API_URL, headers=headers, files={"file": audio_bytes})
+    if response.status_code != 200:
         return {"error": "Error al procesar el audio", "status_code": response.status_code}
-
+    return response.json()
